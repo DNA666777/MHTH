a8c1a34e3e8be40489fd20a4d09341d42ebbf6cf
DNA666777 <kevin.verdier81@gmail.com>
2021-04-12 19:35:31 +0200

    Clean up ignored files

---
+++ MHTH.Front/Controllers/QuestGeneratorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Monster_Hunter_Tabletop_Helper.Controllers
+{
+    using MHTH.Service.Dto;
+    using MHTH.Service.Service;
+
+    public class QuestGeneratorController : Controller
+    {
+        private QuestService QS = new QuestService();
+        // GET: QuestGenerator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GenerateByAverage(int numberPlayer,int averageLevel)
+        {
+            return Json("");
+        }
+        public JsonResult GenerateByCR(decimal CR)
+        {
+            QuestInfoDTO questInfoDTO = this.QS.GenerateQuestByCR(CR);
+
+            return Json(questInfoDTO,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
